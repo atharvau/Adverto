d91e454fca595e241c5f16b8c369f2ed1289a82b
athrva udapure <avu.ceo@gmail.com>
2020-01-21 21:18:17 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,104 @@
+var express = require('express');
+var app = express();
+var fs=require('fs');
+const PORT = process.env.PORT || 3000;
+
+
+var video="jok1.mp4";
+
+app.get('/', function (req, res) {
+    res.sendFile('a.html', {root: __dirname })
+})
+
+app.get('/jok1',function(req,res){
+
+   
+
+    res.send("/Assets/jok2.mp4");
+}
+)
+
+app.get('/jok2',function(req,res){
+
+  
+    res.send("/Assets/jok2.mp4");
+
+
+})
+
+
+
+app.get('/video', function(req, res) {
+
+    const path = `Assets/${video}`
+    const stat = fs.statSync(path)
+    const fileSize = stat.size
+    const range = req.headers.range
+    if (range) {
+      const parts = range.replace(/bytes=/, "").split("-")
+      const start = parseInt(parts[0], 10)
+      const end = parts[1] 
+        ? parseInt(parts[1], 10)
+        : fileSize-1
+      const chunksize = (end-start)+1
+      const file = fs.createReadStream(path, {start, end})
+      const head = {
+        'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+        'Accept-Ranges': 'bytes',
+        'Content-Length': chunksize,
+        'Content-Type': 'video/mp4',
+      }
+      res.writeHead(206, head);
+      file.pipe(res);
+    } else {
+      const head = {
+        'Content-Length': fileSize,
+        'Content-Type': 'video/mp4',
+      }
+      res.writeHead(200, head)
+      fs.createReadStream(path).pipe(res)
+    }
+  });
+
+
+
+app.get('/video2', function(req, res) {
+  
+    const path = `Assets/jok2.mp4`
+    const stat = fs.statSync(path)
+    const fileSize = stat.size
+    const range = req.headers.range
+    if (range) {
+      const parts = range.replace(/bytes=/, "").split("-")
+      const start = parseInt(parts[0], 10)
+      const end = parts[1] 
+        ? parseInt(parts[1], 10)
+        : fileSize-1
+      const chunksize = (end-start)+1
+      const file = fs.createReadStream(path, {start, end})
+      const head = {
+        'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+        'Accept-Ranges': 'bytes',
+        'Content-Length': chunksize,
+        'Content-Type': 'video/mp4',
+      }
+      res.writeHead(206, head);
+      file.pipe(res);
+    } else {
+      const head = {
+        'Content-Length': fileSize,
+        'Content-Type': 'video/mp4',
+      }
+      res.writeHead(200, head)
+      fs.createReadStream(path).pipe(res)
+    }
+  });
+
+
+
+var server = app.listen(PORT, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+})
